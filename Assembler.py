c7e8a03eb43b1fcbca19dfd9c7ed66f035e6d556
Aryan-Dhull <99260658+Aryan-Dhull@users.noreply.github.com>
2022-07-03 15:22:19 +0530

    Update Assembler.py

--- Assembler.py
+++ Assembler.py
@@ -1,5 +1,8 @@
-with open ('input.txt',"r") as f:
-	lines=f.read().splitlines()
+import sys                                    
+lines = sys.stdin.read().splitlines()
+
+# with open ('input.txt',"r") as f:
+# 	lines=f.read().splitlines()
 
 regaddress = {"R0":"000","R1":"001","R2":"010","R3":"011","R4":"100","R5":"101", "R6":"110","FLAGS":"111"}
 
